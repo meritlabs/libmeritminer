dcef213700362b5913faff082d10802d47923204
Eugene Sypachev (@Axblade) <eugene@merit.me>
2019-05-28 23:14:22 +0200

    update license text

--- src/cuckoo/mean_cuckoo.cpp
+++ src/cuckoo/mean_cuckoo.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2013-2018 John Tromp
- * Copyright (C) 2018 The Merit Foundation
+ * Copyright (C) 2018-2019 The Merit Foundation
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
